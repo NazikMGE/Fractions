8b102b558da91c9e0936fccb027b39f32117211e
Nazarii <69088292+NazikMGE@users.noreply.github.com>
2023-04-03 05:53:58 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
+// Підключаємо два заголовочних файли - Fractions.h та Menu.h
 #include "Fractions.h"
 #include "Menu.h"
 
-
+// Головна функція програми
 int main() {
-    Menu menu;
-    menu.mainMenu();
-    return 0;
+	// Створюємо об'єкт класу Menu з назвою menu
+	Menu menu;
+	// Викликаємо функцію mainMenu() з об'єкту menu класу Menu, яка відображає головне меню програми
+	menu.mainMenu();
+	// Повертаємо 0, що означає успішне завершення програми
+	return 0;
 }
